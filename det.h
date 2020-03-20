6570aca63bc0a42a6b333bbd985f47521b0ac5f8
pinkfcpp <pinkfcpp@gmail.com>
2020-03-20 17:02:47 +0600

    Add files via upload

---
+++ det.h
@@ -0,0 +1,33 @@
+#define all(x) x.begin(), x.end()
+
+static ll sign(const vector < int > & p)
+{
+    ll res = 1;
+    for (int i = 0; i < (int)p.size(); i++)
+    {
+        for (int j = 0; j < i; j++)
+            if (p[j] > p[i])
+                res *= -1;
+    }
+    return res;
+}
+
+Polynomial det(vector < vector < Polynomial > > A)
+{
+    int n = (int)A.size();
+    vector < int > p;
+    for (int i = 0; i < n; i++)
+        p.pb(i);
+    Polynomial res;
+    do
+    {
+        Polynomial cur(1);
+        for (int i = 0; i < n; i++)
+            cur *= A[i][p[i]];
+        cur *= sign(p);
+        res += cur;
+    }
+    while(next_permutation(p.begin(), p.end()));
+    res.normalize();
+    return res;
+}
