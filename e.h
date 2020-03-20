4194ebb7cc3d0a9e2e93c3fa0142e511834c8c18
pinkfcpp <pinkfcpp@gmail.com>
2020-03-20 17:53:56 +0600

    Update e.h

--- e.h
+++ e.h
@@ -1,29 +1,37 @@
 
-static map < int, int > character;
-static vector < int > argv;
-static Polynomial ans;
-
-static void rec(int i, int k)
+struct E
 {
-    if (k > (int)argv.size() - i)
-        return;
-    if (i == (int)argv.size())
-    {
-        ans.add(character, 1);
-        return;
-    }
-    if (k > 0)
-    {
-        character[argv[i]]++;
-        rec(i + 1, k - 1);
-        character.erase(argv[i]);
-    }
-    rec(i + 1, k);
-}
+    map < int, int > character;
+    vector < int > argv;
+    Polynomial ans;
 
+    E() {}
+
+    E(vector < int > _argv):argv(_argv) {}
+
+    void rec(int i, int k)
+    {
+        if (k > (int)argv.size() - i)
+            return;
+        if (i == (int)argv.size())
+        {
+            assert(k == 0);
+            ans.add(character, 1);
+            return;
+        }
+        if (k > 0)
+        {
+            character[argv[i]]++;
+            rec(i + 1, k - 1);
+            character.erase(argv[i]);
+        }
+        rec(i + 1, k);
+    }
+};
 Polynomial e(vector < int > _argv, int k)
 {
-    argv = _argv;
-    rec(0, k);
-    return ans;
+    E res(_argv);
+    if (k >= 0)
+        res.rec(0, k);
+    return res.ans;
 }
