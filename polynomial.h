55cb23c39cb59c3a743bc68f6e5ad42ecdb146db
pinkfcpp <pinkfcpp@gmail.com>
2020-03-20 15:09:37 +0600

    Add files via upload

---
+++ polynomial.h
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+struct Polynomial
+{
+    map < map < int, int >, ll > p;
+
+    Polynomial() {};
+
+    Polynomial (int _val) // init as a number
+    {
+        p[map < int, int > ()] = _val;
+    };
+
+    void add(map < int, int > character, ll val)
+    {
+        p[character] += val;
+    }
+
+    bool isPositive()
+    {
+        for (auto it : p)
+        {
+            if (it.second < 0)
+                return false;
+        }
+        return true;
+    }
+
+    map < int, int > mult(map < int, int > a, map < int, int > b)
+    {
+        for (auto it : b)
+        {
+            a[it.first] += it.second;
+        }
+        return a;
+    }
+
+    void print()
+    {
+        bool tmp = false;
+        for (auto it : p)
+        {
+            if (tmp)
+                cout << " + ";
+            cout << it.second << "(";
+            bool kek = false;
+            for (auto a : it.first)
+            {
+                if (kek)
+                    cout << " * ";
+                cout << a.first << "^" << a.second;
+                kek = true;
+            }
+            cout << ")";
+            tmp = true;
+        }
+    }
+
+    void operator += (Polynomial q)
+    {
+        for (auto it : q.p)
+        {
+            p[it.first] += it.second;
+        }
+    };
+
+    void operator *= (Polynomial q)
+    {
+        map < map < int, int >, ll > res;
+        for (auto a : p)
+        {
+            for (auto b : q.p)
+            {
+                res[mult(a.first, b.first)] += a.second * b.second;
+            }
+        }
+        p = res;
+    }
+
+    void operator *= (ll k)
+    {
+        for (auto & it : p)
+            it.second *= k;
+    }
+
+    bool operator == (Polynomial q)
+    {
+        return p == q.p;
+    }
+};
