9aa0809ef0dcc84e6ac11ab0dcca8f53882f53a3
pinkfcpp <pinkfcpp@gmail.com>
2020-04-02 19:44:57 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,40 @@ using namespace std;
 
 int main()
 {
-    TreeDimLGV Lattice;
-    Lattice.addSource(0, 0, -4);
-    Lattice.addSource(1, 0, -3);
-    Lattice.addSink(1, 4, -2);
-    Lattice.addSink(3, 4, -1);
+    /*TreeDimLGV Lattice;
+     Lattice.addSource(0, 0, -4);
+    Lattice.addSource(1, 0, -4);
+    Lattice.addSource(2, 0, -4);
+    Lattice.addSource(4, 0, -4);
+    Lattice.addSink(4, 4, -2);
+    Lattice.addSink(5, 4, -1);
+    Lattice.addSink(6, 4, -1);
+    Lattice.addSink(7, 4, 0);
     Lattice.addEdge(0, 0, 1, FLOOR, true, 1);
     Lattice.addEdge(-1, 0, 1, FLOOR, true, 10);
     Lattice.addEdge(0, 1, 0, WALL, true, 20);
     Lattice.addEdge(1, 1, 0, WALL, true, 30);
-    Lattice.addEdge(1, 0, 0, WALL, true, 40);
-    Polynomial res = Lattice.LGV();
-    if (res.isPositive())
-        cout << "Positive!" << endl;
-    else
-        cout << "Negative!" << endl;
+    Lattice.addEdge(2, 1, 0, WALL, true, 40);
+    Lattice.LGV();
+    return 0;*/
+    srand(time(NULL));
+    bool pos;
+    do
+    {
+        pos = false;
+        TreeDimLGV Lattice;
+        Lattice.randomInit(3);
+    Lattice.addEdge(0, 0, 1, FLOOR, true, 1);
+    Lattice.addEdge(1, 0, 1, FLOOR, true, 10);
+    Lattice.addEdge(1, 1, 0, WALL, true, 20);
+    Lattice.addEdge(0, 1, 0, WALL, true, 30);
+        Polynomial res = Lattice.LGV();
+        //res.print();
+        if (pos = res.isPositive())
+            cout << "Positive!" << endl;
+        else
+            cout << "Negative!" << endl;
+    }
+    while(pos);
+    return 0;
 }
