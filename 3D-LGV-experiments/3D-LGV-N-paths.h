50d04aaa07efe4fe613858fd86f19eaa87316830
pinkfcpp <pinkfcpp@gmail.com>
2020-06-22 23:59:56 +0600

    Update 3D-LGV-N-paths.h

--- 3D-LGV-experiments/3D-LGV-N-paths.h
+++ 3D-LGV-experiments/3D-LGV-N-paths.h
@@ -5,23 +5,37 @@
 // addEdge(Edge e, int id) - same as prev. but weight = x_{id}
 // print() - prints source, and seq. of edges
 
+
+// struct NPath: {permutation, weight, {list of Path}}
+// generator: NPath(vector < int > permutation, vector < Path > paths) - calculates weight by 
+//																		 multiplying weights of each path in paths
+//
+// generator: NPath() - creates an empty Paths, but with weight = 1
+// functions:
+// print() - prints ;)
+// addPath(int start, int finish, Path p) - sets perm[start] = finish, paths[start] = p
+
+
 struct Path
 {
     int source_x, source_y, source_z;
     Polynomial weight;
     vector < Edge > edges;
 
-    Path () {}
+    Path () 
+    {
+    	weight = Polynomial(1);
+    }
 
     Path (int x, int y, int z): source_x(x), source_y(y), source_z(z), weight(1) {}
 
-    void addEdge(Edge e, Polynomial w)
+    void addEdge(Edge e, Polynomial w) // adds edge e with weight w
     {
         edges.pb(e);
         weight *= w;
     }
 
-    void addEdge(Edge e, int id)
+    void addEdge(Edge e, int id) // adds edge e with weight x_{id}
     {
         edges.pb(e);
         weight *= Xpower(id, 1);
@@ -36,6 +50,51 @@ struct Path
             cout << '(' << e.dx << ',' << e.dy << ',' << e.dz << ") ";
         cout << endl << "--------------" << endl;
     }
+};
+
+struct NPath 
+{
+	vector < int > perm;
+	Polynomial weight;
+	vector < Path > paths;
+
+	NPath () 
+	{		
+		weight = Polynomial(1);
+	};
+
+	NPath (vector < int > _perm, vector < Path > _paths) // calculates weight by multiplying weight of path
+	{
+		perm = _perm;
+		paths = _paths;
+		weight = Polynomial(1);
+		for (auto it : paths)
+			weight *= it.weight;
+	}
+
+	void addPath(int s, int f, Path p)
+	{
+		perm.resize(max((int)perm.size(), s + 1));
+		paths.resize(max((int)paths.size(), s + 1));
+		
+		perm[s] = f;
+		paths[s] = p;		
+	}
+
+	void print()
+	{
+		cout << "========N-Path print===================" << endl;
+		cout << "Permutation" << endl << "A: ";
+		for (int i = 0; i < (int)perm.size(); i++)
+			cout << i << ' ';
+		cout << endl << "B: ";	
+		for (int i = 0; i < (int)perm.size(); i++)
+			cout << perm[i] << ' ';
+		cout << endl;
+		for (auto it : paths)
+			it.print();
+		cout << "=======================================" << endl;	
+	}
 };
 
 
