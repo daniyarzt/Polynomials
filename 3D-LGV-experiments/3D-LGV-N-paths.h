833956f4c8f935f45506c02019d0efd7d4f43b29
pinkfcpp <pinkfcpp@gmail.com>
2020-06-23 15:25:46 +0600

    Update 3D-LGV-N-paths.h

--- 3D-LGV-experiments/3D-LGV-N-paths.h
+++ 3D-LGV-experiments/3D-LGV-N-paths.h
@@ -10,7 +10,7 @@
 /*  *** struct NPath *** 
 	struct NPath: {permutation, weight, {list of Path}}
 	generator: NPath(vector < int > permutation, vector < Path > paths) - calculates weight by 
-				multiplying weights of each path in paths
+																		 multiplying weights of each path in paths
 
 	generator: NPath(vector < int > permutation) -  generates NPath with given permutation, paths are empty, weight = 1
 	generator: NPath() - creates an empty Paths, but with weight = 1
@@ -20,20 +20,10 @@
 	getSign() - returns the sign of permutation
 
 	vector < NPath > mult(vector <  NPath >, vector < Path >, int start, int finish) - does the same thing as polynomial multiplication
-		, but now with saving paths
+																		, but now with saving paths
 */
 
-/*  *** struct PathSavingLGV	
-	Is Used to calculate Lindstrom-Gessel-Vienot Lemma with saving each path in the lattice
-
-	mult(vector < NPath > a, vector < Path > b, int start, int finish) - does the same thing as polynomial multiplication, 
-									but now with saving paths (a * b)
-	
-	add (vector < NPath > a, vector < NPath > b) - same as polynomial addition 
-
-	det(D) - does the same thing as the polynomial det, but also saves paths
-	
-*/																			
+																		
 
 struct Path
 {
@@ -103,7 +93,8 @@ struct NPath
 			paths.resize(s + 1);
 		
 		perm[s] = f;
-		paths[s] = p;		
+		paths[s] = p;
+		weight *= p.weight;		
 	}
 
 	int getSign()  // returns the sign on the permutation
@@ -130,59 +121,6 @@ struct NPath
 			it.print();
 		cout << "=======================================" << endl;	
 	}
-};
-
-struct PathSavingLGV
-{
-	// mult(,,,) - does the same thing as polynomial multiplication, but now with saving paths
-	vector < NPath > mult(vector < NPath > vec, vector < Path > vpath, int start, int finish) 
-	{
-		vector < NPath > newvec;
-		for (auto p : vpath)
-		{
-			for (auto it : vec)
-			{
-				it.addPath(start, finish, p);
-				newvec.pb(it);
-			}
-		}
-		return newvec;
-	}
-
-	// same as polynomial addition 
-	vector < NPath > add(vector < NPath > a, vector < NPath > b)
-	{
-		for (auto it : b)
-			a.pb(it);
-		return a;
-	}
-
-	// det(D) - does the same thing as the polynomial det, but saves paths
-	vector < NPath > det(vector < vector < vector < Path > > > D) 
-	{
-		int n = (int)D.size();
-		
-		vector < int > p(n);
-		for (int i = 0; i < n; i++)
-			p[i] = i;	
-
-		vector < NPath > res;
-			
-		do
-		{
-			vector < NPath > cur(1, p);
-			for (int i = 0; i < n; i++)
-			{
-				//D[i][p[i]]: i -> p[i]
-				cur = mult(cur, D[i][p[i]], i, p[i]);		
-			}	
-			res = add(res, cur);			
-		}
-		while(next_permutation(all(p)));
-
-		return res;
-	}
-
 };
 
 
